d82b4b7a879e2c113af51a9e02a6d20cd9548417
Anany More <anany.more@ananymoress-475.successivetech.com>
2025-06-09 16:45:04 +0530

    refactored all files

--- Features/assignment-2/question5.js
+++ Features/assignment-2/question5.js
@@ -1,12 +1,10 @@
 // 5. Write a function expression that takes in two numbers and returns their sum.
 
-const firstNumber = Number(prompt("Enter First Number"));
-const secondNumber = Number(prompt("Enter Second Number"));
+const firstNumber = Number(prompt("Enter First Number: "));
+const secondNumber = Number(prompt("Enter Second Number: "));
 
-const sumFunction = (firstNumber, secondNumber) => {
-  const sum = firstNumber + secondNumber;
-  return sum;
-};
+const sumFunction = (firstNumber, secondNumber) => (firstNumber + secondNumber)
+
 
 const finalResult = sumFunction(firstNumber, secondNumber);
 console.log(`The sum of 1st number and 2nd number is : ${finalResult}`);
