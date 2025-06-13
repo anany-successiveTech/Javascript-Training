9d3fc2fa5efff575ccc36a656ccac690ced04605
Anany More <anany.more@ananymoress-475.successivetech.com>
2025-06-13 16:17:39 +0530

    Removed error causing part

--- Features/assignment-4/question10.js
+++ Features/assignment-4/question10.js
@@ -1,15 +1,15 @@
 // 10. Write a program to concatenate an input of array of arrays
 
 
-const concatenateArray = (array) => {
+const concatenateArray = () => {
     const inputArray = [ [1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12] ];
     const concatenatedArray = [];
-    for(let i = 0; i < array.length; i++){
-        for(let j = 0; j < array[i].length; j++){
-            concatenatedArray.push(array[i][j]);
+    for(let i = 0; i < inputArray.length; i++){
+        for(let j = 0; j < inputArray[i].length; j++){
+            concatenatedArray.push(inputArray[i][j]);
         }
     }
     return concatenatedArray;
 };
 
-console.log(`The concatenated array is: ${concatenateArray(inputArray)}`);
+console.log(`The concatenated array is: ${concatenateArray()}`);
