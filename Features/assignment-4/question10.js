02358bc1ae31b205889e47006936b679bd30784b
Anany More <anany.more@ananymoress-475.successivetech.com>
2025-06-13 16:00:31 +0530

    Moved both array inside

--- Features/assignment-4/question10.js
+++ Features/assignment-4/question10.js
@@ -1,9 +1,8 @@
 // 10. Write a program to concatenate an input of array of arrays
 
-const inputArray = [ [1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12] ];
-
 
 const concatenateArray = (array) => {
+    const inputArray = [ [1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12] ];
     const concatenatedArray = [];
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < array[i].length; j++){
